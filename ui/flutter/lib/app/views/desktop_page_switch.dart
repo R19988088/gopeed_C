06083415c9d4c8f0a02b08cb8b20b91b6629a5f5
ddd <ddd@ddddeMacBook-Air.local>
2026-07-06 12:23:24 +0800

    Revert "Simplify desktop page switch styling"
    This reverts commit bc0d22023b3153cc5ac0441ae483cf4d44243b44.

--- ui/flutter/lib/app/views/desktop_page_switch.dart
+++ ui/flutter/lib/app/views/desktop_page_switch.dart
@@ -14,15 +14,6 @@ class DesktopPageSwitch extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    const borderColor = Color(0xFF3D7E3A);
-    const shadow = [
-      BoxShadow(
-        color: Color(0x33000000),
-        blurRadius: 5,
-        offset: Offset(0, 2),
-      ),
-    ];
-
     return Align(
       alignment: Alignment.topCenter,
       child: Row(
@@ -33,13 +24,12 @@ class DesktopPageSwitch extends StatelessWidget {
             height: 42,
             margin: const EdgeInsets.only(bottom: 6),
             decoration: BoxDecoration(
-              border: Border.all(color: borderColor, width: 1.5),
+              border: Border.all(color: const Color(0xFF3D7E3A)),
               borderRadius: BorderRadius.circular(10),
-              boxShadow: shadow,
             ),
             clipBehavior: Clip.antiAlias,
             child: TabBar(
-              indicator: const BoxDecoration(color: borderColor),
+              indicator: const BoxDecoration(color: Color(0xFF3D7E3A)),
               labelColor: Colors.white,
               unselectedLabelColor: Colors.white,
               labelStyle: const TextStyle(
@@ -53,14 +43,9 @@ class DesktopPageSwitch extends StatelessWidget {
           const SizedBox(width: 8),
           Padding(
             padding: const EdgeInsets.only(bottom: 6),
-            child: Container(
+            child: SizedBox(
               width: 42,
               height: 42,
-              decoration: BoxDecoration(
-                border: Border.all(color: borderColor, width: 1.5),
-                borderRadius: BorderRadius.circular(10),
-                boxShadow: shadow,
-              ),
               child: IconButton(
                 icon: const Icon(Icons.more_horiz),
                 onPressed: onAction,
